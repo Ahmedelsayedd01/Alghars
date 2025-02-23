56ce9cffd9b31e35c016c27b51732d8dffea898b
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2025-02-23 19:05:29 +0200

    customize app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./index.css";
 import { Outlet } from "react-router-dom";
 import { useAuth } from "./Context/Auth";
 import { Navbar, Sidebar } from "./Components/Components";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-blue/theme.css";
 
 const App = () => {
   const auth = useAuth();
@@ -9,29 +11,33 @@ const App = () => {
 
   return (
     <>
-      <div className="relative w-full flex h-screen overflow-hidden bg-secoundBgColor">
-        {/* Sidebar */}
-        <div
-          className={`${
-            hideSide ? "w-60" : "w-16"
-          } fixed right-0 z-10 duration-300 overflow-hidden`}
-        >
-          <Sidebar />
-        </div>
+      <PrimeReactProvider>
+        <div className="relative w-full flex h-screen overflow-hidden bg-secoundBgColor">
+          {/* Sidebar */}
+          <div
+            className={`${
+              hideSide ? "w-60" : "w-16"
+            } fixed right-0 z-10 duration-300 overflow-hidden`}
+          >
+            <Sidebar />
+          </div>
 
-        {/* Main Content */}
-        <div className={`${hideSide ? "pr-60" : "pr-16"} w-full duration-300`}>
-          {/* Navbar */}
-          <div className="sticky top-0 left-0 z-10 bg-secoundBgColor drop-shadow-nav">
-            <Navbar />
-          </div> 
+          {/* Main Content */}
+          <div
+            className={`${hideSide ? "pr-60" : "pr-16"} w-full duration-300`}
+          >
+            {/* Navbar */}
+            <div className="sticky top-0 left-0 z-10 bg-secoundBgColor drop-shadow-nav">
+              <Navbar />
+            </div>
 
-          {/* Main Content Area */}
-          <div className="relative w-full px-3 h-full overflow-y-scroll scrollPage">
-            <Outlet />
+            {/* Main Content Area */}
+            <div className="relative w-full px-3 h-full overflow-y-scroll scrollPage">
+              <Outlet />
+            </div>
           </div>
         </div>
-      </div>
+      </PrimeReactProvider>
     </>
   );
 };
