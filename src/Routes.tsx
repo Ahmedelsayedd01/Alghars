9781ec827d4e7e92ae6c12662e47059d84d333a7
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2025-02-23 19:04:05 +0200

    customize route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,18 +2,65 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import ProtectedLogin from "./ProtectedAuth/ProtectedLogin";
 import { LoginPage } from "./Pages/Pages";
+import ProtectedRoute from "./ProtectedAuth/ProtectedRoute";
+import {
+  ClassesLayout,
+  StudentsLayout,
+  SubjectsLayout,
+  TeachersLayout,
+} from "./Layouts/Layouts";
 
 export const router = createBrowserRouter(
   [
     /* Login */
     {
       path: "/login",
-      element: <ProtectedLogin />,
+      element: (
+        <ProtectedLogin>
+          <LoginPage />
+        </ProtectedLogin>
+      ),
       children: [{ path: "", element: <LoginPage /> }],
     },
     {
       path: "/",
-      element: <App />,
+      element: (
+        <>
+          <ProtectedLogin>
+            <App />
+          </ProtectedLogin>
+        </>
+      ),
+      children: [
+        /* Admin */
+        {
+          path: "/dashboard",
+          // element: <ProtectedRoute />,
+          children: [
+            {
+              path: "teachers",
+              element: <TeachersLayout />,
+            },
+            {
+              path: "students",
+              element: <StudentsLayout />,
+            },
+            {
+              path: "subjects",
+              element: <SubjectsLayout />,
+            },
+            {
+              path: "classes",
+              element: <ClassesLayout />,
+            },
+          ],
+        },
+        /* Teacher */
+        // {
+        //   path:'',
+        //   element:<TeacerClasses/>
+        // }
+      ],
     },
   ],
   {
