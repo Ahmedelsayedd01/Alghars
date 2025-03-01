49d329f9c30d8b2a3dba97822a6122e9193d2501
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2025-03-01 17:22:57 +0200

    add schedul layout

---
+++ src/Layouts/Teacher/ScheduleTeacher/SchedulesLayout.tsx
@@ -0,0 +1,11 @@
+import { SchedulesPage } from "../../../Pages/Pages";
+
+const SchedulesLayout = () => {
+  return (
+    <>
+      <SchedulesPage />
+    </>
+  );
+};
+
+export default SchedulesLayout;
